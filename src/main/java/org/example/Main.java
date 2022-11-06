87904d6e6c6214e65d0182994c691ba0bc503334
vikashkumarvikash99@gmail.com <>
2022-11-06 11:58:51 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! i am vikash");
+        int  x,y,z;
+        y=x=5;
+        z=y+x;
+        System.out.println("Hello world! i am vikash how are you,5+5="+z);
     }
 }
